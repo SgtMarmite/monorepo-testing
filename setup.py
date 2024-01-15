fdbffb7d91f7c9246b37a90ea68f08c08288e16a
SgtMarmite <d.novotny31415@gmail.com>
2024-01-15 17:36:41 +0100

    start with version 0.0.1

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ project_urls = {
 
 setuptools.setup(
     name="kebola.DBComponent",
-    version="0.0.2",
+    version="0.0.1",
     author="Keboola KDS Team",
     project_urls=project_urls,
     setup_requires=['pytest-runner', 'flake8'],
